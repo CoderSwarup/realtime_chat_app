24fa112ad7ac48c5e061724cb7950d23dfa0fb65
Swarup Bhise <swarupbhise420@gmail.com>
2024-02-08 00:36:32 +0530

    Create a SideBar in Client version 2

--- client-v2/src/pages/dashboard/GeneralApp.jsx
+++ client-v2/src/pages/dashboard/GeneralApp.jsx
@@ -1,12 +1,8 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
+const Cat = lazy(() => import("../../components/Cat"));
 
 const GeneralApp = () => {
-
-  return (
-    <>
-      App
-    </>
-  );
+  return <>{/* <h1>Hello</h1> */}</>;
 };
 
 export default GeneralApp;
