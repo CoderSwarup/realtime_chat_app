579267994b9d019df8d0c0f518d5bdee8a35e4c9
Swarup Bhise <swarupbhise420@gmail.com>
2024-02-10 23:42:18 +0530

    Creating the conversation Component header and Footer

--- client-v2/src/pages/dashboard/GeneralApp.jsx
+++ client-v2/src/pages/dashboard/GeneralApp.jsx
@@ -1,12 +1,29 @@
 import React, { Suspense, lazy } from "react";
 import Chats from "./Chats";
+import { Box, Stack, useTheme } from "@mui/material";
+import Conversation from "../../components/Conversation";
 const Cat = lazy(() => import("../../components/Cat"));
 
 const GeneralApp = () => {
+  const theme = useTheme();
   return (
-    <>
+    <Stack direction="row" sx={{ width: "100%" }}>
       <Chats />
-    </>
+      {/* Conversation  */}
+
+      <Box
+        sx={{
+          height: "100%",
+          width: "calc(100vw - 410px)",
+          background:
+            theme.palette.mode == "light"
+              ? "#FAFAFE"
+              : theme.palette.background.default,
+        }}
+      >
+        <Conversation />
+      </Box>
+    </Stack>
   );
 };
 
